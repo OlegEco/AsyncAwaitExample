6632ec09724d3b8dd77e06b66fb7900735f3e391
Oleg <100606637+OlegEco@users.noreply.github.com>
2024-07-27 13:01:29 +0200

    Add project files.

---
+++ AsyncAwaitExample/Program.cs
@@ -0,0 +1,15 @@
+﻿string url = "https://jsonplaceholder.typicode.com/posts";
+string data = await FetchDataAsync(url);
+Console.WriteLine(data);
+
+async Task<string> FetchDataAsync(string url)
+{
+    using (HttpClient client = new HttpClient())
+    {
+        HttpResponseMessage response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        string responseData = await response.Content.ReadAsStringAsync();
+        return responseData;
+
+    }
+}
